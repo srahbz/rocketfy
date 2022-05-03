0e554ba290d06b25f4560b36107c9689534f7f92
Sarah Benzi <sarahlover822@gmail.com>
2022-05-03 11:28:06 -0300

    style das tabelas &+div

--- src/components/list/styles.js
+++ src/components/list/styles.js
@@ -4,4 +4,10 @@ export const Container = styled.div`
   padding: 0 15px;
   height: 100%;
   flex: 0 0 320px;
+
+  /* quero estilizar toda div que antes dela tenha uma div */
+  & + div{
+      border-left:2px solid rgba(70,10,105, 0.5)
+
+  }
 `;
