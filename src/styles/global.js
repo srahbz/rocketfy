9981cea8c3a266d8d144da54b22f5fbcea8f7850
Sarah Benzi <sarahlover822@gmail.com>
2022-05-03 09:15:27 -0300

    font importada

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,1 +1,6 @@
-import {createStyledComponent } from 'styled-components'
+import {createGlobalStyle } from 'styled-components'
+
+
+export default createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap');
+`
