05f97340868ceae0a71a87df637afd80e4e41625
Sarah Benzi <sarahlover822@gmail.com>
2022-05-03 09:44:33 -0300

    criação dos componentes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import  GlobalStyle  from "./styles/global";
 
 function App() {
   return (
+      // fragment é como uma div mas facilita a estilizacao
     <Fragment>
       <GlobalStyle/>
     </Fragment>
