62867083812cecc27ab0c21f89c0b1e8ab55ca77
Sarah Benzi <sarahlover822@gmail.com>
2022-05-03 10:56:06 -0300

    adicionado o header do card e o texto do card

--- src/components/cards/index.js
+++ src/components/cards/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { Container,Label } from './styles';
 
 function cards() {
-  return <div />;
+  return (
+  <Container >
+    <header>
+      {/* é o quadradinhoo colorido */}
+      <Label color="blueviolet"/>
+    </header>
+    {/* é o conteudo escrito */}
+    <p>
+      Fazer migração completa de servidor
+    </p>
+  </Container>
+  );
 }
 
 export default cards;
