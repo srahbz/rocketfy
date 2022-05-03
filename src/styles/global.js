35ad3c6a1bb178640b185da5a3e74f2663002946
Sarah Benzi <sarahlover822@gmail.com>
2022-05-03 09:19:11 -0300

    styles que vao ser aplicados a todos os elementos implementado

--- src/styles/global.js
+++ src/styles/global.js
@@ -3,4 +3,12 @@ import {createGlobalStyle } from 'styled-components'
 
 export default createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap');
+
+/* stylo p todos os elementos */
+*{
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+}
 `
