8d626df10e4bd0dd73ca7677877b8bcb52cac01e
Sarah Benzi <sarahlover822@gmail.com>
2022-05-03 11:12:58 -0300

    style da imagem do card implementada

--- src/components/cards/styles.js
+++ src/components/cards/styles.js
@@ -1,6 +1,13 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  
+  img{
+      height: 30px;
+      width: 30px;
+      border: solid 3px orange;
+      border-radius: 50px;
+      margin-top: 5px;
+
+  }
 `;
 export const Label = styled.span``;
